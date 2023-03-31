c866dacb003ef9e3304b349bdb9bb626c47908d6
Saanykeys <125324312+Saanykeys@users.noreply.github.com>
2023-03-31 10:19:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+const ITEMS_CONTAINER = document.getElementById("items");
+const ITEM_TEMPLATE = document.getElementById("itemTemplate");
+const ADD_BUTTON = document.getElementById("add");
+
+let items = getItems();
+
+function getItems() {
+    const value = localStorage.getItem("todo-test") || "[]";
+
+    return JSON.parse(value);
+}
+
+function setItems(items) {
+    const itemsJson = Json.stringify(items);
+
+    localStorage.setItem("todo-test", itemsJson);
+}
+
+function addItem() {
+    items.unshift({
+        description: "",
+        completed: false
+    });
+
+    setItems(items); {
+    refreshList();
+}
+
+function updateItem(item, key, value) {
+    item[key] = value;
+    setItems(items);
+    refreshList(list);
+}
+    
+
+    ITEMS_CONTAINER.innerHTML = "";
+
+
+    for (const item of items) {
+        const itemElement = ITEM_TEMPLATE.content.cloneNode(true);
+        const descriptionInput = itemElement.querySelector(".item-description");
+        const completedInput = itemElement.querySelector(".item-completed");
+
+        descriptionInput.value = item.description; 
+        completedInput.checked = item.completed;
+
+        descriptionInput.addEventListener("change", () => {
+           updateItem(item, "desciption", descriptionInput.value)
+        });
+
+        completedInput.addEventListener("change", () => {
+            updateItem(item, "completed", completedInput.checked);
+         });
+ 
+
+        ITEMS_CONTAINER.append(itemElement);
+    }
+}
+
+ADD_BUTTON.addEventListener("click", () => {
+    addItem();
+});
+
+
+refreshList();
+
